d295347fb29426822c27ffc07add8383716819e8
Joey Nigro <cujojoeynigro@gmail.com>
2023-02-14 07:23:10 -0600

    Fixed

--- poptropica.bat
+++ poptropica.bat
@@ -1,3 +1,3 @@
 cd %CD%\server\
-start "" "%CD%\browser\chrome.exe" --new-window "http://localhost:9999/"
+start "" "%CD%\..\browser\chrome.exe" --new-window "http://localhost:9999/"
 cmd /k npm start
